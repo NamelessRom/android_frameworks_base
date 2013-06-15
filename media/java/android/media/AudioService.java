a80cd7dbcccff12f348065a03f66c6707f44a92d
Danny Baumann <dannybaumann@web.de>
2013-06-15 02:39:11 +0200

    Fix ringer mode being unneededly changed by setting ringer mode volume to 0.
    When having set the ringer mode to silent on a device with vibrator and
    calling setStreamVolume() for the ringer stream again (e.g. in one of
    the volume control dialogs in the settings app), the ringer mode is set
    to vibrate, although there's no reason to do so.
    
    Fix that by remembering the last non-normal (that is, either silent or
    vibrate) ringer mode and using that when setting the stream volume to 0.
    
    Change-Id: I27f1c75a0fef52bd63ac20f4101042808ce2c458
    JIRA: CYAN-1403

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -303,6 +303,8 @@ public class AudioService extends IAudioService.Stub {
      */
     // protected by mSettingsLock
     private int mRingerMode;
+    // last non-normal ringer mode
+    private int mLastSilentRingerMode = -1;
 
     /** @see System#MODE_RINGER_STREAMS_AFFECTED */
     private int mRingerModeAffectedStreams = 0;
@@ -1000,8 +1002,14 @@ public class AudioService extends IAudioService.Stub {
                 (mStreamVolumeAlias[streamType] == getMasterStreamType())) {
             int newRingerMode;
             if (index == 0) {
-                newRingerMode = mHasVibrator ? AudioManager.RINGER_MODE_VIBRATE
-                                              : AudioManager.RINGER_MODE_SILENT;
+                synchronized (mSettingsLock) {
+                    if (mLastSilentRingerMode != -1) {
+                        newRingerMode = mLastSilentRingerMode;
+                    } else {
+                        newRingerMode = mHasVibrator ? AudioManager.RINGER_MODE_VIBRATE
+                            : AudioManager.RINGER_MODE_SILENT;
+                    }
+                }
             } else {
                 newRingerMode = AudioManager.RINGER_MODE_NORMAL;
             }
@@ -1428,6 +1436,9 @@ public class AudioService extends IAudioService.Stub {
     private void setRingerModeInt(int ringerMode, boolean persist) {
         synchronized(mSettingsLock) {
             mRingerMode = ringerMode;
+            if (ringerMode != AudioManager.RINGER_MODE_NORMAL) {
+                mLastSilentRingerMode = ringerMode;
+            }
         }
 
         // Mute stream if not previously muted by ringer mode and ringer mode
