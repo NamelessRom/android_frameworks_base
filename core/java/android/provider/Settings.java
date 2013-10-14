d533bc79a65a4179716dd899c0c98b5bd271b662
Jorge Ruesga <jorge@ruesga.com>
2013-10-14 02:24:55 +0200

    Frameworks: QS performance profiles tile
    Patchset 2: Fix not null comparation
    Patchset 3: xxhdpi drawables
    Patchset 4: Fix xxhdpi drawables (thx blunden)
                Rebased
    Patchset 5: Add suggestions
                Sync on boot the system property with the setting
    Patchset 6: Rebased
    
    Change-Id: Ib32d3734b63f88a3473e0ab10d8edf30dae9ceb6
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3620,6 +3620,13 @@ public final class Settings {
         public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
 
         /**
+         * Performance profile
+         * @see config_perf_profile_prop in frameworks/base/core/res/res/values/config.xml
+         * @hide
+         */
+        public static final String PERFORMANCE_PROFILE = "performance_profile";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
