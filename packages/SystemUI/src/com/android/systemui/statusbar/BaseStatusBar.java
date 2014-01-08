a05315430d76e99a48ca5427e48489e245e9390f
BigBrother1984 <stevewatersy@gmail.com>
2014-01-08 18:24:11 +0100

    [WIP] Quicksettings 3.0
    * Improve code (cleanup, indenting, redistribute resources, some comments updates).
    * Fix: Restore stock aosp transition for quick settings on rotate (the rebuild animation
           executed when you are on quick settings panel and rotate the screen is not always showed).
    * Animated Dual Tiles
      Animation: Use flip animator classes by xplodwild (OmniRom).
      Use aosp quick settings classes and methods to build dual tiles.
      Make dedicated classes for rssi/network tile.
      Change the original flippable tiles concept making them dual tiles for real.
      Integrate dual tiles into edit mode (TESTING).
      Tiles added:
          -Wifi AP Tile (Dual): Made the aosp way. Created receiver for the main intent sent to change
           wifi ap state (also toggling with apps will refresh it). Integrated with Wifi Tile. Thanks
           CyanogenMod for the drawables.
          -Screen Timeout Tile (Dual): Integrate it in the sleep tile. Cleaner implement. Thanks Yanuar Harry
           for improving the behaviour :)
          -Bluetooth Extra Tile (Dual): Original from OmniRom. Rewrote it. Build and update it with android’s
           stock classes and methods (aosp basic tile's). Toggles discoverability. Thanks Lo Huot for the drawables.
      Tiles cleaned/updated:
          -Immersive Tile: Cleanup and store its resources to QuickSettingsModel with their own methods. Setup a post delay runnable/handler
           to let we postpone the activating animation and let quicksettings panel finish the collapse.
          -Brightness Tile: Long click toggles auto-mode.
          -RSSI Tile (Dual): Simplify the code. Show off indicator when data is disabled. Merge in Mobile Networks
           Tile.
          -Cleaned Wifi one and general cleanup for the others.
    * TODO: Dual tiles drag listener don't work in edit mode, btw if you drag another one (normal) onto it they got exchanged.
    * TODO: Fix missing signal icon in rssi front tile.
    
    PS2: Fix rssi front tile construction.
    PS3: Clean up wrongly added file.
         Fix and update rssi tile.
         Solve recent merge conflicts.
    PS4: Rebase:
         Fix traces of not solved conflicts after recent merges rebase.
         Improve the dual tile logic, reduce code in QuickSettings class.
         Improve screen timeout (sleep sub-tile) code, thanks Yanuar Harry.
         Don't return dual (flip) animator touch event on edit mode. (Need test)
         Nitpicks cleanup.
         Update main commit message.
    PS5: Continue rebasing:
         General code cleanup.
         Improve strings and drawables labels.
         Fix wifi ap tile construction and NPEs.
         Remove unused imports.
         Cleaned dual sleep tile implement (more polish methods names).
         Cleaned paired device bluetooth extra tile traces (added some todo).
         Update edit mode integration.
         Added a refresh callback sub-class for network tile (now contructed with
             new Basic Network Tile class Not yet used).
         Update commit message (added a TODO for rssi tile and for edit mode).
    PS6: Finish bluetooth extra tile. Add drawables, refresh resources.
         Improve immersive tile.
         Traces of previous rebase.
    
    Change-Id: Iceccd6a489c23286d8bc4e285fbb87b77509cd29

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -16,6 +16,9 @@
 
 package com.android.systemui.statusbar;
 
+import android.service.notification.StatusBarNotification;
+import android.content.res.Configuration;
+import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
 import android.app.Notification;
@@ -33,6 +36,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
 import android.database.ContentObserver;
+import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Build;
@@ -51,6 +55,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.util.Slog;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.IWindowManager;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -73,12 +78,17 @@ import com.android.internal.statusbar.StatusBarIconList;
 import com.android.internal.widget.SizeAdaptiveLayout;
 import com.android.systemui.R;
 import com.android.systemui.RecentsComponent;
+import com.android.systemui.recent.RecentTasksLoader;
+import com.android.systemui.recent.RecentsActivity;
+import com.android.systemui.recent.TaskDescription;
 import com.android.systemui.SearchPanelView;
 import com.android.systemui.SystemUI;
 import com.android.systemui.statusbar.phone.KeyguardTouchDelegate;
+import com.android.systemui.statusbar.phone.PhoneStatusBar;
 import com.android.systemui.statusbar.policy.NotificationRowLayout;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public abstract class BaseStatusBar extends SystemUI implements
@@ -96,6 +106,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected static final int MSG_SHOW_HEADS_UP = 1026;
     protected static final int MSG_HIDE_HEADS_UP = 1027;
     protected static final int MSG_ESCALATE_HEADS_UP = 1028;
+    protected static final int MSG_TOGGLE_LAST_APP = 1029;
 
     protected static final boolean ENABLE_HEADS_UP = true;
     // scores above this threshold should be displayed in heads up mode.
@@ -459,47 +470,25 @@ public abstract class BaseStatusBar extends SystemUI implements
                 mNotificationBlamePopup.getMenuInflater().inflate(
                         R.menu.notification_popup_menu,
                         mNotificationBlamePopup.getMenu());
-                final ContentResolver cr = mContext.getContentResolver();
-                if (Settings.Secure.getInt(cr,
-                        Settings.Secure.DEVELOPMENT_SHORTCUT, 0) == 0) {
-                    mNotificationBlamePopup.getMenu()
-                            .findItem(R.id.notification_inspect_item_force_stop).setVisible(false);
-                    mNotificationBlamePopup.getMenu()
-                            .findItem(R.id.notification_inspect_item_wipe_app).setVisible(false);
-                } else {
-                    try {
-                        PackageManager pm = (PackageManager) mContext.getPackageManager();
-                        ApplicationInfo mAppInfo = pm.getApplicationInfo(packageNameF, 0);
-                        DevicePolicyManager mDpm = (DevicePolicyManager) mContext.
-                                getSystemService(Context.DEVICE_POLICY_SERVICE);
-                        if ((mAppInfo.flags&(ApplicationInfo.FLAG_SYSTEM
-                              | ApplicationInfo.FLAG_ALLOW_CLEAR_USER_DATA))
-                              == ApplicationInfo.FLAG_SYSTEM
-                              || mDpm.packageHasActiveAdmins(packageNameF)) {
-                            mNotificationBlamePopup.getMenu()
-                            .findItem(R.id.notification_inspect_item_wipe_app).setEnabled(false);
-                        }
-                    } catch (NameNotFoundException ex) {
-                        Slog.e(TAG, "Failed looking up ApplicationInfo for " + packageNameF, ex);
+
+                MenuItem hideIconCheck = mNotificationBlamePopup.getMenu().findItem(R.id.notification_hide_icon_packages);
+                if(hideIconCheck != null) {
+                    hideIconCheck.setChecked(isIconHiddenByUser(packageNameF));
+                    if (packageNameF.equals("android")) {
+                        // cannot set it, no one likes a liar 
+                        hideIconCheck.setVisible(false);
                     }
                 }
 
-                mNotificationBlamePopup
-                .setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                mNotificationBlamePopup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     public boolean onMenuItemClick(MenuItem item) {
                         if (item.getItemId() == R.id.notification_inspect_item) {
                             startApplicationDetailsActivity(packageNameF);
                             animateCollapsePanels(CommandQueue.FLAG_EXCLUDE_NONE);
-                        } else if (item.getItemId() == R.id.notification_inspect_item_force_stop) {
-                            ActivityManager am = (ActivityManager) mContext
-                                    .getSystemService(
-                                    Context.ACTIVITY_SERVICE);
-                            am.forceStopPackage(packageNameF);
-                        } else if (item.getItemId() == R.id.notification_inspect_item_wipe_app) {
-                            ActivityManager am = (ActivityManager) mContext
-                                    .getSystemService(Context.ACTIVITY_SERVICE);
-                            am.clearApplicationUserData(packageNameF,
-                                    new FakeClearUserDataObserver());
+                        } else if (item.getItemId() == R.id.notification_hide_icon_packages) {
+                            item.setChecked(!item.isChecked());
+                            setIconHiddenByUser(packageNameF, item.isChecked());
+                            updateNotificationIcons();
                         } else {
                             return false;
                         }
@@ -531,6 +520,12 @@ public abstract class BaseStatusBar extends SystemUI implements
     @Override
     public void toggleRecentApps() {
         int msg = MSG_TOGGLE_RECENTS_PANEL;
+        mHandler.removeMessages(msg);
+        mHandler.sendEmptyMessage(msg);
+    }
+
+    public void toggleLastApp() {
+        int msg = MSG_TOGGLE_LAST_APP;
         mHandler.removeMessages(msg);
         mHandler.sendEmptyMessage(msg);
     }
@@ -606,6 +601,10 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     protected abstract View getStatusBarView();
 
+    protected void toggleLastAppImpl() {
+        TaskUtils.toggleLastApp(mContext);
+    }
+
     protected View.OnTouchListener mRecentsPreloadOnTouchListener = new View.OnTouchListener() {
         // additional optimization when we have software system buttons - start loading the recent
         // tasks on touch down
@@ -669,6 +668,10 @@ public abstract class BaseStatusBar extends SystemUI implements
              case MSG_CANCEL_PRELOAD_RECENT_APPS:
                   cancelPreloadingRecentTasksList();
                   break;
+             case MSG_TOGGLE_LAST_APP:
+                 if (DEBUG) Log.d(TAG, "toggle last app");
+                 toggleLastAppImpl();
+                 break;
              case MSG_OPEN_SEARCH_PANEL:
                  if (DEBUG) Log.d(TAG, "opening search panel");
                  if (mSearchPanelView != null) {
@@ -1232,3 +1235,4 @@ public abstract class BaseStatusBar extends SystemUI implements
         mContext.unregisterReceiver(mBroadcastReceiver);
     }
 }
+
