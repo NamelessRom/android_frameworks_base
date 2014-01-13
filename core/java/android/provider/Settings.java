0f960cfbe85512f1033d4ac71e9f8426cc042c09
Adam Fisch <adammfisch@gmail.com>
2014-01-13 09:08:57 +0100

    [1/2] Clock mode
    Center, right, hidden clock.
    AM/PM and weekday mods.
    
    PS3: Fix memory leak
    
    Change-Id: I5b62e2e3e0909f15f1a7edb8fa8ad50b0ed7c1f8
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7491,6 +7491,39 @@ public final class Settings {
         public static final String CERT_PIN_UPDATE_CONTENT_URL = "cert_pin_content_url";
 
         /**
+         * AM/PM Style for clock options
+         * 0 - Normal AM/PM
+         * 1 - Small AM/PM
+         * 2 - No AM/PM
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_AM_PM_STYLE = "statusbar_clock_am_pm_style";
+
+        /**
+         * Style of clock
+         * 0 - Hide Clock
+         * 1 - Right Clock
+         * 2 - Center Clock
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_STYLE = "statusbar_clock_style";
+
+        /**
+         * Setting for clock color
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_COLOR = "statusbar_clock_color";
+
+        /**
+         * Shows weekday before clock time
+         * 0 - No Day
+         * 1 - Small Day
+         * 2 - Normal Day
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_WEEKDAY = "statusbar_clock_weekday";
+
+        /**
          * URL for cert pinlist updates
          * @hide
          */
