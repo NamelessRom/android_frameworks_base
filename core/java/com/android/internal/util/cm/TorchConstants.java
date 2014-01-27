82f16a5ec8d2b59e45d9514064184cc0ef3f7fbf
Jake Whatley <jake@nameless-rom.org>
2014-01-26 22:46:52 -0500

    fix Flashlight Tile
    for now, it's quite annoying that it doesn't work since we changed packages
    
    Change-Id: Iccb933a3d66a8c6a3910d1236388ac0ce2ec0604

--- core/java/com/android/internal/util/cm/TorchConstants.java
+++ core/java/com/android/internal/util/cm/TorchConstants.java
@@ -22,31 +22,19 @@ public class TorchConstants {
     /**
      * Package name of the torch app
      */
-    public static final String APP_PACKAGE_NAME = "net.cactii.flash2";
-
-    /**
-     * Intent broadcast action for toggling the torch state
-     */
-    public static final String ACTION_TOGGLE_STATE = APP_PACKAGE_NAME + ".TOGGLE_FLASHLIGHT";
-
-    /**
-     * Extra for {@link ACTION_TOGGLE_STATE}:
-     * When toggling to on, use the bright brightness setting
-     * Type: boolean
-     */
-    public static final String EXTRA_BRIGHT_MODE = "bright";
+    public static final String APP_PACKAGE_NAME = "com.nameless.flashlight";
 
     /**
      * Intent action for 'torch state changed' broadcast
      */
-    public static final String ACTION_STATE_CHANGED = APP_PACKAGE_NAME + ".TORCH_STATE_CHANGED";
+    public static final String ACTION_STATE_CHANGED = "TORCH_STATUS_CHANGED";
 
     /**
-     * Extra for {@link ACTION_STATE_CHANGED}:
+     * Extra for {@link ACTION_STATUS_CHANGED}:
      * Current torch state
      * Type: integer (0/1)
      */
-    public static final String EXTRA_CURRENT_STATE = "state";
+    public static final String EXTRA_CURRENT_STATE = "TORCH_MODE";
 
     /**
      * Intent for launching the torch application
