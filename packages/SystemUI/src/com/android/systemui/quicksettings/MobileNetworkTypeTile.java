bbc2446720266816cfe39d25ca7f3a74c932b13d
Langes <langes72@gmail.com>
2014-02-05 23:11:17 +0200

    QS: give animation love to network mode and wifi tiles
    Change-Id: I59dcede96ce5895e3ce38c29f36c9f08b11e3599

--- packages/SystemUI/src/com/android/systemui/quicksettings/MobileNetworkTypeTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/MobileNetworkTypeTile.java
@@ -89,6 +89,9 @@ public class MobileNetworkTypeTile extends QuickSettingsTile implements NetworkS
 
                 mMode = NETWORK_MODE_UNKNOWN;
                 mContext.sendBroadcast(intent);
+                if (isFlipTilesEnabled()) {
+                    flipTile(0);
+                }
             }
         };
 
