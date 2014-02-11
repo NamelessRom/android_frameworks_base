8008b96c2c2b6299cb8cb0ad29d6c1e77caded70
Jubakuba <Jubakuba@gmail.com>
2014-02-11 01:38:42 -0500

    [1/3] Base: Glowpad Torch
    PS2: Torch active? Keep screen on.
    
    Modified for NamelessROM
    
    PS7: fix package name derp and remove unused stuff from FlashlightConstant
    s
    Change-Id: Icd5d3dbe76317c6be4e378fc5219d2b5db52f45b

---
+++ core/java/com/android/internal/util/nameless/FlashlightConstants.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ * Copyright (C) 2013 ParanoidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.nameless;
+
+import android.content.Intent;
+
+public class FlashlightConstants {
+    /**
+     * Package name of the flashlight app
+     */
+    public static final String APP_PACKAGE_NAME = "com.nameless.flashlight";
+
+    /**
+     * Intent broadcast action for toggling the torch state
+     */
+    public static final String ACTION_TOGGLE_STATE = APP_PACKAGE_NAME + ".TOGGLE_FLASHLIGHT";
+
+    /**
+     * Intent for launching the torch application
+     */
+    public static Intent INTENT_LAUNCH_APP = new Intent(Intent.ACTION_MAIN)
+            .setClassName(APP_PACKAGE_NAME, APP_PACKAGE_NAME + ".MainActivity");
+}
