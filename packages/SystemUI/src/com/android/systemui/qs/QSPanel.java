a911fa657b46495d1e880c5273763eca529ed2f6
Alexander Martinz <eviscerationls@gmail.com>
2014-02-13 18:35:47 +0100

    SystemUI: add haptic feedback to quick tiles
    Change-Id: Ide5fa24009d5135accf6eccb077757ec22ad1e82
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -28,6 +28,7 @@ import android.os.Message;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.AttributeSet;
+import android.view.HapticFeedbackConstants;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -124,6 +125,7 @@ public class QSPanel extends ViewGroup {
                 announceForAccessibility(
                         mContext.getString(R.string.accessibility_desc_quick_settings));
                 closeDetail();
+                performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
             }
         });
     }
@@ -340,18 +342,21 @@ public class QSPanel extends ViewGroup {
             @Override
             public void onClick(View v) {
                 r.tile.click();
+                performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
             }
         };
         final View.OnClickListener clickSecondary = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 r.tile.secondaryClick();
+                performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
             }
         };
         final View.OnLongClickListener longClick = new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
                 r.tile.longClick();
+                performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
                 return true;
             }
         };
@@ -423,6 +428,7 @@ public class QSPanel extends ViewGroup {
                 @Override
                 public void onClick(View v) {
                     mHost.startActivityDismissingKeyguard(settingsIntent);
+                    performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
                 }
             });
 
