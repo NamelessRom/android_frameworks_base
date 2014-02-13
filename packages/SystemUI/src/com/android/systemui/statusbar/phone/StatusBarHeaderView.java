c3a4a9f5ce58635c5c5b288e90ac5654e27f2ef7
Alexander Martinz <eviscerationls@gmail.com>
2014-02-13 18:35:47 +0100

    SystemUI: add haptic feedback to quick tiles
    Change-Id: Ide5fa24009d5135accf6eccb077757ec22ad1e82
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -45,6 +45,7 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.util.MathUtils;
 import android.util.TypedValue;
+import android.view.HapticFeedbackConstants;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewOutlineProvider;
@@ -580,7 +581,11 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             startDateActivity();
         } else if (v == mWeatherContainer) {
             startForecastActivity();
+        } else {
+            return;
         }
+
+        performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
     }
 
     private void startSettingsActivity() {
