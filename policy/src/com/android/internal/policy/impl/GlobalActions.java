141ed828806b2c4cf175bf90506f051362422f30
jumoog <kilianvonpflugk@gmail.com>
2014-03-29 20:56:27 +0100

    Lockscreen Notifications: some small fixes
    Change-Id: I77665e37d823c619680312b73d5ebb1e6e437ffd
    
    Conflicts:
    	services/java/com/android/server/NotificationManagerService.java

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -1311,7 +1311,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     PhoneStateListener mPhoneStateListener = new PhoneStateListener() {
         @Override
         public void onServiceStateChanged(ServiceState serviceState) {
-            if (!mHasTelephony || mAirplaneModeOn == null || mAdapter == null) return;
+            if (!mHasTelephony) return;
             final boolean inAirplaneMode = serviceState.getState() == ServiceState.STATE_POWER_OFF;
             mAirplaneState = inAirplaneMode ? ToggleAction.State.On : ToggleAction.State.Off;
             mHandler.sendEmptyMessage(MESSAGE_REFRESH_AIRPLANEMODE);
