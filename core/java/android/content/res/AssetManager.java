ddba2c847d2ecd8bb5815287557cc36eedfe97ac
Andy Mast <andy@cyngn.com>
2014-04-25 16:04:43 -0700

    Allow icons in shared restables to be themed
    Example
    Google Drive's package  name: com.google.android.apps.docs
    Google Drive's restable name: com.google.android.apps.docs.common
    
    Before this patch we used resource table name to get the package info. For
    app's like google drive this return a null package info and the icon would not be themed.
    
    So instead of usnig the restable name, this patch creates a package name
    in AssetManager that is set by getTopLevelResources.
    
    Change-Id: Ic51b93f6c7ea0a32197682998c6d0aa36d774845

--- core/java/android/content/res/AssetManager.java
+++ core/java/android/content/res/AssetManager.java
@@ -823,12 +823,10 @@ public final class AssetManager {
         mThemeCookies.add(cookie);
     }
 
-    /** {@hide} */
     public String getAppName() {
         return mAppName;
     }
 
-    /** {@hide} */
     public void setAppName(String pkgName) {
         mAppName = pkgName;
     }
