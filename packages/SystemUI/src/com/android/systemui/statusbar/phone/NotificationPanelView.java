a15c4fba07b123e35cafa0c02dc7dfd11f82d03c
PrimeDirective <activethrasher00@gmail.com>
2014-05-16 13:39:56 -0400

    Block signal cluster updates during swipe to notifications/QS
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
    
    Change-Id: Ib6336659534614e9ff42efa78381ed3b1e3b0538

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -68,7 +68,7 @@ public class NotificationPanelView extends PanelView {
     private float mFlipOffset;
     private float mSwipeDirection;
     private boolean mTrackingSwipe;
-    private boolean mSwipeTriggered;
+    private static boolean mSwipeTriggered;
 
     public NotificationPanelView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -274,6 +274,10 @@ public class NotificationPanelView extends PanelView {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
             setNotificationWallpaper();
+    }
+
+    public static boolean isAnimating() {
+        return mSwipeTriggered;
     }
 
     private void setNotificationWallpaper() {
