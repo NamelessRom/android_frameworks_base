e5841df063c495c4cea5525ebf84b73c294c3231
David96 <xD_e-mail@gmx.de>
2014-06-13 23:56:40 +0200

    LN: Fix FC on launching hangouts notification
    Change-Id: Idf411bd2b4995f0b53767e99597efd98ce0596b3

--- packages/Keyguard/src/com/android/keyguard/NotificationHostView.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationHostView.java
@@ -185,6 +185,9 @@ public class NotificationHostView extends FrameLayout {
         public void onClick(View v) {
             PendingIntent i = statusBarNotification.getNotification().contentIntent;
             if (!swipeGesture && !longpress && i != null) {
+                if ((statusBarNotification.getNotification().flags & Notification.FLAG_AUTO_CANCEL) != 0) {
+                    dismiss(statusBarNotification);
+                }
                 try {
                     Intent intent = i.getIntent();
                     intent.setFlags(
@@ -195,9 +198,6 @@ public class NotificationHostView extends FrameLayout {
                     ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
                     i.send();
                     hideAllNotifications();
-                    if ((statusBarNotification.getNotification().flags & Notification.FLAG_AUTO_CANCEL) != 0) {
-                        removeNotification(statusBarNotification);
-                    }
                 } catch (CanceledException ex) {
                     Log.e(TAG, "intent canceled!");
                 } catch (RemoteException ex) {
