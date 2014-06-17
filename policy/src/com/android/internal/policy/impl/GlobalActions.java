a050c8361d76d0b6edee0474a107175bbed21755
Evisceration <eviscerationls@gmail.com>
2014-06-17 19:23:22 +0200

    Revert "Revert "[1/3] base: Audio Themes Support""
    This reverts commit 98859fe70488bf850057f5178d9ff8555ed84ad4.
    
    Change-Id: Ieddc5a39d2d08e45bdf6b4f993814ec4ff0e4d30

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -77,6 +77,7 @@ import android.widget.ImageView.ScaleType;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.internal.app.ThemeUtils;
 import com.android.internal.util.nameless.NamelessActions;
 
 import java.util.ArrayList;
