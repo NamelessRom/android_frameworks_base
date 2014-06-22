f40ffcb95d52e5a55a6892c6fa94623846d7bd46
Alexander Martinz <eviscerationls@gmail.com>
2014-06-22 03:13:11 +0200

    Revert "ProgressBar: Fix error of process bar cannot update on some condition."
    This reverts commit d70bcfbe30126c4e25831299494108ece6fc506f.
    
    Change-Id: I7adc3c071b7d8574027fe89729cde97f10fde7c6

--- core/java/android/widget/ProgressBar.java
+++ core/java/android/widget/ProgressBar.java
@@ -1184,7 +1184,9 @@ public class ProgressBar extends View {
         }
         if (mRefreshProgressRunnable != null) {
             removeCallbacks(mRefreshProgressRunnable);
-            mRefreshIsPosted = false;
+        }
+        if (mRefreshProgressRunnable != null && mRefreshIsPosted) {
+            removeCallbacks(mRefreshProgressRunnable);
         }
         if (mAccessibilityEventSender != null) {
             removeCallbacks(mAccessibilityEventSender);
