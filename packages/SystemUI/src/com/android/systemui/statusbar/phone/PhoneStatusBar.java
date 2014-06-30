b88ab85d8de15d944b73aedfa7f8ffcda0edd06f
Adnan <adnan@cyngn.com>
2014-06-30 13:48:02 -0700

    PhoneStatusBar: Readd headsup view when theme change occurs.
    Change-Id: I5bbfa56650a1b2f9cd9c4365650f769bed009ab0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3481,6 +3481,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         makeStatusBarView();
         repositionNavigationBar();
+        addHeadsUpView();
         if (mNavigationBarView != null) {
             mNavigationBarView.updateResources();
         }
