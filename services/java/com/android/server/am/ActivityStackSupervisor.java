616f35214d8a51140dd7030bdf85e439e2183850
Leo Hsu <Leo_Hsu@htc.com>
2014-07-03 16:51:24 +0800

    Fix a bug: unable to start activity when AM is killing the process.
    Symptom: No activity was started for startActivity, low repro rate.
    Root Cause: Starting activity on a process which was killed by ActivityManagerService.killUnneededProcessLocked() and before receiving AppDeathRecipient of it.
    Solution: Choose the flow of starting a new process if ProcessRecord.killedByAm is true.
    
    Change-Id: Ida2639b0fb4631222fc92d65aadc9fd2da637b45

--- services/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/java/com/android/server/am/ActivityStackSupervisor.java
@@ -1055,7 +1055,7 @@ public final class ActivityStackSupervisor {
 
         r.task.stack.setLaunchTime(r);
 
-        if (app != null && app.thread != null) {
+        if (app != null && app.thread != null && !app.killedByAm) {
             try {
                 if ((r.info.flags&ActivityInfo.FLAG_MULTIPROCESS) == 0
                         || !"android".equals(r.info.packageName)) {
