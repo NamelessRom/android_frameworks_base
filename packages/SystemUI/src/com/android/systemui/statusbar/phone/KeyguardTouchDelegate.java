e204e645f8e2a55ad3af240a2895e57f4118b4c4
Danesh Mondegarian <daneshm90@gmail.com>
2014-07-26 20:23:04 -0700

    LockscreenTile : Don't rely on DEVICE_POLICY broadcast
    The current trigger used for initialization is too finicky and doesn't
    work on some devices. Switch to trigger based on instantiation of the service
    inside the delegate instead.
    
    Also fix inverted drawable state.
    
    Change-Id: I8439ed50281de805ae68fe7b5a81220475d98022

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardTouchDelegate.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardTouchDelegate.java
@@ -200,8 +200,4 @@ public class KeyguardTouchDelegate {
             Slog.w(TAG, "dismiss(): NO SERVICE!");
         }
     }
-
-    public boolean isServiceInitialized() {
-        return mService != null;
-    }
 }
