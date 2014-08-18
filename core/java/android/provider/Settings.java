022dceb5b752ef81438282447860d80aa6728e5d
Peter Gregus <pete.gregus@gmail.com>
2014-08-18 17:28:32 +0200

    (1/2) TrafficMeter: option for auto-hiding on inactivity
    Change-Id: I60a6568470ad075e337628ed34db02f73aff1148

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4117,6 +4117,18 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
 
         /**
+         * Whether or not to hide the network traffic indicator when there is no activity
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE = "network_traffic_autohide";
+
+        /**
+         * Network traffic inactivity threshold (default is 10 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
+        /**
          * Give SMS notifications a breathing effect
          * @hide
          */
