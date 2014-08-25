23df33fe488468d2c053ddfc1a0efec8610ab5d9
Roman Birg <roman@cyngn.com>
2014-08-25 15:35:40 -0700

    frameworks: allow last app action to be assigned to hardware keys
    Also fix multi-user handling for 'switch to last app' and 'kill
    foreground app' actions.
    
    Change-Id: I1fb586d19b1198e6888b0f11e0a2813acf0bcd5b
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: Danny Baumann <dannybaumann@web.de>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -73,14 +73,13 @@ import android.os.UserHandle;
 import android.os.Vibrator;
 import android.provider.Settings;
 
-import com.android.internal.util.cm.DevUtils;
-import com.android.internal.util.nameless.NamelessActions;
-
 import android.service.dreams.DreamService;
 import android.service.dreams.IDreamManager;
 import android.service.gesture.EdgeGestureManager;
 import com.android.internal.os.DeviceKeyHandler;
 
+import com.android.internal.util.cm.ActionUtils;
+import com.android.internal.util.nameless.NamelessActions;
 import dalvik.system.DexClassLoader;
 
 import android.util.DisplayMetrics;
@@ -1132,7 +1131,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private final Runnable mBackLongPress = new Runnable() {
         public void run() {
-            if (DevUtils.killForegroundApplication(mContext)) {
+            if (ActionUtils.killForegroundApp(mContext, mCurrentUserId)) {
                 performHapticFeedbackLw(null, HapticFeedbackConstants.LONG_PRESS, false);
                 Toast.makeText(mContext, R.string.app_killed_message, Toast.LENGTH_SHORT).show();
             }
@@ -1208,7 +1207,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 launchCameraAction();
                 break;
             case KEY_ACTION_LAST_APP:
-                toggleLastApp();
+                ActionUtils.switchToLastApp(mContext, mCurrentUserId);
                 break;
             case KEY_ACTION_IMMERSIVE_MODE:
                 toggleImmersiveMode();
