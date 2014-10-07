00725b27999b47f4251f40ed807f8629042ae41c
sssemil <suleymanovemil8@gmail.com>
2014-10-07 16:15:06 +0500

    Customisable location tile(2/2)
    Change-Id: Id7a8a9522e8222ac990ebc13ae4b227c153d5ed1

--- packages/SystemUI/src/com/android/systemui/quicksettings/GPSTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/GPSTile.java
@@ -4,6 +4,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.view.LayoutInflater;
@@ -17,12 +18,23 @@ import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
 import com.android.systemui.statusbar.policy.LocationController;
 import com.android.systemui.statusbar.policy.LocationController.LocationSettingsChangeCallback;
 
+import java.util.Arrays;
+
 
 public class GPSTile extends QuickSettingsTile implements LocationSettingsChangeCallback {
 
     ContentResolver mContentResolver;
     private LocationController mLocationController;
     private int mCurrentMode;
+    private int mLocatorsMode;
+    private int mLocatorsIndex;
+
+    private static final int[] LOCATORS = new int[]{
+            Settings.Secure.LOCATION_MODE_OFF,
+            Settings.Secure.LOCATION_MODE_BATTERY_SAVING,
+            Settings.Secure.LOCATION_MODE_SENSORS_ONLY,
+            Settings.Secure.LOCATION_MODE_HIGH_ACCURACY
+    };
 
     public GPSTile(Context context, QuickSettingsController qsc, LocationController lc) {
         super(context, qsc);
@@ -34,7 +46,8 @@ public class GPSTile extends QuickSettingsTile implements LocationSettingsChange
         mOnClick = new OnClickListener() {
             @Override
             public void onClick(View v) {
-                changeLocationMode();
+                toggleState();
+                updateResources();
             }
         };
 
@@ -45,40 +58,14 @@ public class GPSTile extends QuickSettingsTile implements LocationSettingsChange
                 return true;
             }
         };
-
-        mCurrentMode = Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.LOCATION_MODE, Settings.Secure.LOCATION_MODE_OFF);
-        updateTile();
-    }
-
-    private void changeLocationMode(){
-        int newMode;
-
-        switch(mCurrentMode){
-        case Settings.Secure.LOCATION_MODE_BATTERY_SAVING:
-            newMode = Settings.Secure.LOCATION_MODE_HIGH_ACCURACY;
-            break;
-        case Settings.Secure.LOCATION_MODE_HIGH_ACCURACY:
-            newMode = Settings.Secure.LOCATION_MODE_BATTERY_SAVING;
-            break;
-        case Settings.Secure.LOCATION_MODE_OFF:
-            newMode = Settings.Secure.LOCATION_MODE_SENSORS_ONLY;
-            break;
-        case Settings.Secure.LOCATION_MODE_SENSORS_ONLY:
-            newMode = Settings.Secure.LOCATION_MODE_OFF;
-            break;
-        default:
-            newMode = Settings.Secure.LOCATION_MODE_OFF;
-            break;
-        }
-
-        Settings.Secure.putInt(mContext.getContentResolver(),
-                Settings.Secure.LOCATION_MODE,
-                newMode);
+        qsc.registerObservedContent(
+                Settings.System.getUriFor(Settings.System.EXPANDED_LOCATION_MODE), this);
     }
 
     @Override
     void onPostCreate() {
+        updateSettings();
+        onLocationSettingsChanged(false);
         updateTile();
         super.onPostCreate();
     }
@@ -93,6 +80,52 @@ public class GPSTile extends QuickSettingsTile implements LocationSettingsChange
     public void updateResources() {
         updateTile();
         updateQuickSettings();
+    }
+
+    @Override
+    public void onLocationSettingsChanged(boolean locationEnabled) {
+        mCurrentMode = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.LOCATION_MODE, Settings.Secure.LOCATION_MODE_OFF,
+                UserHandle.USER_CURRENT);
+        mLocatorsIndex = currentModeToLocatorIndex(mCurrentMode);
+        updateResources();
+    }
+
+    private void toggleState() {
+        int locatorIndex = mLocatorsIndex;
+        int mask = 0;
+        do {
+            locatorIndex++;
+            if (locatorIndex >= LOCATORS.length) {
+                locatorIndex = 0;
+            }
+            mask = (int) Math.pow(2, locatorIndex);
+        } while (mLocatorsMode > 1 && (mLocatorsMode & mask) != mask); // Off is always preset
+
+        // Set the desired state
+        Settings.Secure.putIntForUser(mContext.getContentResolver(),
+                Settings.Secure.LOCATION_MODE, LOCATORS[locatorIndex], UserHandle.USER_CURRENT);
+    }
+
+    private int currentModeToLocatorIndex(int mode) {
+        int count = LOCATORS.length;
+        for (int i = 0; i < count; i++) {
+            if (LOCATORS[i] == mode) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    private void updateSettings() {
+        mLocatorsMode = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.EXPANDED_LOCATION_MODE, 0, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public void onChangeUri(ContentResolver resolver, Uri uri) {
+        updateSettings();
+        updateResources();
     }
 
     private synchronized void updateTile() {
@@ -118,10 +151,4 @@ public class GPSTile extends QuickSettingsTile implements LocationSettingsChange
         mLabel = mContext.getText(textResId).toString();
     }
 
-    @Override
-    public void onLocationSettingsChanged(boolean locationEnabled) {
-        mCurrentMode = Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.LOCATION_MODE, Settings.Secure.LOCATION_MODE_OFF);
-        updateResources();
-    }
 }
