9179a28fce3f91d7d3d5a2b884ddc4f7ed2c1315
Evisceration <eviscerationls@gmail.com>
2014-10-23 15:39:44 +0200

    (1/2) Gesture Panel
    Change-Id: Ia7d4de4dd62e016910c01a1b04736de5c8adb9da

--- core/java/com/android/internal/util/nameless/NamelessActions.java
+++ core/java/com/android/internal/util/nameless/NamelessActions.java
@@ -1,21 +1,21 @@
 /*
-* <!--
-*    Copyright (C) 2014 The NamelessROM Project
-*
-*    This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-* -->
-*/
+ * <!--
+ *    Copyright (C) 2014 The NamelessROM Project
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * -->
+ */
 
 package com.android.internal.util.nameless;
 
@@ -25,24 +25,26 @@ import android.content.Intent;
 import android.os.PowerManager;
 import android.os.SystemClock;
 import android.os.UserHandle;
+import android.util.Log;
 
 public class NamelessActions {
+    private static final String TAG = "NamelessActions";
 
-    public static final String ACTION_ONTHEGO_TOGGLE = "action_onthego_toggle";
-    public static final String ACTION_SCREEN_RECORD  = "action_screen_record";
-
-    public static void processAction(final Context context, final String action) {
-
-        if (action == null || action.isEmpty()) {
-            return;
+    public static void processAction(final Context c, final ActionConstants.NamelessAction action) {
+        switch (action) {
+            case ACTION_ONTHEGO_TOGGLE:
+                actionOnTheGoToggle(c);
+                break;
+            case ACTION_SCREEN_RECORD:
+                actionScreenRecord(c);
+                break;
+            case ACTION_TOGGLE_GESTURE_PANEL:
+                c.sendBroadcast(new Intent(Intent.ACTION_TOGGLE_GESTURE_PANEL));
+                break;
+            default:
+                Log.w(TAG, "--> action not found");
+                break;
         }
-
-        if (ACTION_ONTHEGO_TOGGLE.equals(action)) {
-            actionOnTheGoToggle(context);
-        } else if (ACTION_SCREEN_RECORD.equals(action)) {
-            actionScreenRecord(context);
-        }
-
     }
 
     private static void actionOnTheGoToggle(final Context context) {
