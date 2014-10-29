931c7001976e35f4910be44600b4e0aa0e5a45bb
Evisceration <eviscerationls@gmail.com>
2014-10-29 22:11:31 +0100

    WallpaperCropper: do crop!
    Change-Id: I2a9571e8648b168e370bf4244bd28d0a65650572

--- packages/WallpaperCropper/src/com/android/wallpapercropper/WallpaperCropActivity.java
+++ packages/WallpaperCropper/src/com/android/wallpapercropper/WallpaperCropActivity.java
@@ -336,7 +336,7 @@ public class WallpaperCropActivity extends Activity {
             }
         };
         cropTask.setOnEndRunnable(onEndCrop);
-        cropTask.setNoCrop(true);
+        cropTask.setNoCrop(false);
         cropTask.execute();
     }
 
