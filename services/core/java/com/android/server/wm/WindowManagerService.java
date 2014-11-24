a35d5ecdd02f86cb0a5e40240b368d7f51ee2e3e
Evisceration <eviscerationls@gmail.com>
2014-11-24 22:36:33 +0100

    framework: increase overall animation speed a little bit by default
    Change-Id: Iae84882b3f972e8f840ffac9691810aeb0def9e4

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -605,9 +605,9 @@ public class WindowManagerService extends IWindowManager.Stub
     PowerManager mPowerManager;
     PowerManagerInternal mPowerManagerInternal;
 
-    float mWindowAnimationScaleSetting = 1.0f;
-    float mTransitionAnimationScaleSetting = 1.0f;
-    float mAnimatorDurationScaleSetting = 1.0f;
+    float mWindowAnimationScaleSetting = 0.85f;
+    float mTransitionAnimationScaleSetting = 0.85f;
+    float mAnimatorDurationScaleSetting = 0.85f;
     boolean mAnimationsDisabled = false;
 
     final InputManagerService mInputManager;
@@ -10749,7 +10749,7 @@ public class WindowManagerService extends IWindowManager.Stub
         return mPolicy.hasNavigationBar();
     }
 
-    @Override 
+    @Override
     public boolean needsNavigationBar() {
         return mPolicy.needsNavigationBar();
     }
