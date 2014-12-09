26e0fc3fb0fc23fd82643727390fcc3e35fb9f27
Kyrylo Mikos <kiril.mik.os@gmail.com>
2014-12-09 13:25:29 +0200

    frameworks/base: Fix key remapping after toggling navigationbar.
    Change-Id: I8069e3106601caf3990cbcdc5056ffc64ddf7bcd

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1631,8 +1631,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 updateWakeGestureListenerLp();
             }
 
-            updateKeyAssignments();
-
             final boolean useEdgeService = Settings.System.getIntForUser(resolver,
                     Settings.System.USE_EDGE_SERVICE_FOR_GESTURES, 1, UserHandle.USER_CURRENT) == 1;
             if (useEdgeService ^ mUsingEdgeGestureServiceForGestures && mSystemReady) {
@@ -1651,6 +1649,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (devForceNavbar != mDevForceNavbar) {
                 mDevForceNavbar = devForceNavbar;
             }
+
+            updateKeyAssignments();
 
             // Configure rotation lock.
             int userRotation = Settings.System.getIntForUser(resolver,
