36bd26745e96d9a79b89fc66289737e3b2a3cc2d
maxwen <max.weninger@gmail.com>
2014-12-13 22:46:01 +0100

    base: added flashlight toggle
    send broadcast com.android.systemui.TOGGLE_FLASHLIGHT to
    toggle the flashlight
    
    Change-Id: I0601bbfb05b3b28b2bedde2602b26e3ec61c27e7

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1135,6 +1135,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_SCREEN_ON);
         filter.addAction(Intent.ACTION_KEYGUARD_WALLPAPER_CHANGED);
+        filter.addAction("com.android.systemui.TOGGLE_FLASHLIGHT");
         if (DEBUG_MEDIA_FAKE_ARTWORK) {
             filter.addAction("fake_artwork");
         }
@@ -3477,6 +3478,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 }
             } else if (Intent.ACTION_KEYGUARD_WALLPAPER_CHANGED.equals(action)) {
                 updateMediaMetaData(true);
+            } else if ("com.android.systemui.TOGGLE_FLASHLIGHT".equals(action)) {
+                mFlashlightController.toggleFlashlight();
             }
         }
     };
