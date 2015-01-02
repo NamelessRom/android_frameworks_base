6b67e75881326aabdb19617294bf666fc3958d8e
Jorge Martín <angel.arasthel@gmail.com>
2015-01-02 12:10:42 +0100

    Add customization for Quick Settings [1/2]
    Allow Quick Settings tiles to be arranged any way you want, delete them or add some.
    
    I removed the "dual" status that was hardcoded into Wifi and BT tiles and added it
    as a variable which is true when the tile is on the first row.
    
    Also, the first row is now hardcoded to have only 2 items to follow the same design as before.
    
    Inversion and Hotspot tile are no longer contextual and can be added, rmeoved or rearranged at will.
    
    Settings: http://review.cyanogenmod.org/83418
    
    Change-Id: Ie596a54a5c4eb777782fb047496cfcd0a6cd2e89

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5547,6 +5547,13 @@ public final class Settings {
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
+         * Use "main" tiles on the first row of the quick settings panel
+         * 0 = no, 1 = yes
+         * @hide
+         */
+        public static final String QS_USE_MAIN_TILES = "sysiu_qs_main_tiles";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
