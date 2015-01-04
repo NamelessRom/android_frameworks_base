11d105c957a77d1d42fbdfb003973e783fa7dd90
Alexander Martinz <eviscerationls@gmail.com>
2015-01-04 16:32:46 +0100

    service: pm: Show packagename during dexopt
    Change-Id: I500d0ce3fddf775e83d4d4bfba2e4519aed57778
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4942,10 +4942,10 @@ public class PackageManagerService extends IPackageManager.Stub {
             Log.i(TAG, "Optimizing app " + curr + " of " + total + ": " + pkg.packageName);
         }
         if (!isFirstBoot()) {
+            final String bootMsg = String.format("%s\n(%s)", mContext.getResources()
+                    .getString(R.string.android_upgrading_apk, curr, total), pkg.packageName);
             try {
-                ActivityManagerNative.getDefault().showBootMessage(
-                        mContext.getResources().getString(R.string.android_upgrading_apk,
-                                curr, total), true);
+                ActivityManagerNative.getDefault().showBootMessage(bootMsg, true);
             } catch (RemoteException e) {
             }
         }
