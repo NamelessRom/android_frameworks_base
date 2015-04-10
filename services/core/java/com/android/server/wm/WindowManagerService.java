652bae9d73196a685885aeecb4865665f629a45c
lion0738 <lion0738@naver.com>
2015-04-10 13:18:33 +0900

    (Squashed) Lock screen see through and blur
    -----
    
    Frameworks: Lockscreen see through (2/3)
    
    Change-Id: Ia558b051e72c8f0503337ec65a41dd5a1097d730
    Signed-off-by: lion0738 <lion0738@naver.com>
    
    ---
    
    Frameworks: Lockscreen blur (2/3)
    
    credits to David96
    
    Change-Id: I226b0ca155e76a803730c1f7464300eb1f46b312
    Signed-off-by: lion0738 <lion0738@naver.com>
    
    ---
    
    Frameworks: Lockscreen blur fix & tweak
    
    Immutable bitmap was passed to renderscript.
    I'm really curious why this code worked before android 6.0.
    
    Added alternative way to blur lockscreen for devices with renderscript problem.
    Grabbed blur code from https://github.com/kikoso/android-stackblur
    
    Moved all blur related code to Blur.java
    
    Change-Id: Id0ddadeb322508ae6fd279a8a243af40d87fd19a
    
    -----
    
    Change-Id: If2e89d602c31c63b5499b193395a6c043a327e15
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -131,6 +131,7 @@ import com.android.server.am.BatteryStatsService;
 import com.android.server.input.InputManagerService;
 import com.android.server.policy.PhoneWindowManager;
 import com.android.server.power.ShutdownThread;
+import namelessrom.providers.NamelessSettings;
 
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
@@ -590,6 +591,10 @@ public class WindowManagerService extends IWindowManager.Stub
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(mDisplayInversionEnabledUri, false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(NamelessSettings.System.getUriFor(
+                    NamelessSettings.System.LOCKSCREEN_SEE_THROUGH), false, this,
+                    UserHandle.USER_ALL);
+            update();
         }
 
         @Override
@@ -599,6 +604,13 @@ public class WindowManagerService extends IWindowManager.Stub
             } else if (mDisplayInversionEnabledUri.equals(uri)) {
                 updateCircularDisplayMaskIfNeeded();
             }
+            update();
+        }
+
+        private void update() {
+            WindowAnimator.sSeeThroughEnabled = NamelessSettings.System.getInt(
+                    mContext.getContentResolver(),
+                    NamelessSettings.System.LOCKSCREEN_SEE_THROUGH, 0) != 0;
         }
     }
 
