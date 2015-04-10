4f981c0b1540313154f6c1a9ede20e72d653f258
lion0738 <lion0738@naver.com>
2015-04-10 13:18:33 +0900

    (Squashed) Lock screen see through and blur
    -----
    
    Frameworks: Lockscreen see through (2/3)
    
    Change-Id: Ia558b051e72c8f0503337ec65a41dd5a1097d730
    Signed-off-by: lion0738 <lion0738@naver.com>
    
    ---
    
    Frameworks: Lockscreen blur (2/3)
    
    credits to David96
    
    Change-Id: I226b0ca155e76a803730c1f7464300eb1f46b312
    Signed-off-by: lion0738 <lion0738@naver.com>
    
    ---
    
    Frameworks: Lockscreen blur fix & tweak
    
    Immutable bitmap was passed to renderscript.
    I'm really curious why this code worked before android 6.0.
    
    Added alternative way to blur lockscreen for devices with renderscript problem.
    Grabbed blur code from https://github.com/kikoso/android-stackblur
    
    Moved all blur related code to Blur.java
    
    Change-Id: Id0ddadeb322508ae6fd279a8a243af40d87fd19a
    
    -----
    
    Change-Id: If2e89d602c31c63b5499b193395a6c043a327e15
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -16,7 +16,6 @@
 
 package com.android.systemui.statusbar.phone;
 
-
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.NonNull;
@@ -189,6 +188,7 @@ import com.android.systemui.volume.VolumeComponent;
 import cyanogenmod.app.CMContextConstants;
 import cyanogenmod.app.CustomTileListenerService;
 import cyanogenmod.app.StatusBarPanelCustomTile;
+import org.namelessrom.internal.utils.Blur;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -216,6 +216,7 @@ import static com.android.systemui.statusbar.phone.BarTransitions.MODE_WARNING;
 
 import cyanogenmod.providers.CMSettings;
 import cyanogenmod.themes.IThemeService;
+import namelessrom.providers.NamelessSettings;
 
 public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         DragDownHelper.DragDownCallback, ActivityStarter, OnUnlockMethodChangedListener,
@@ -442,6 +443,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     // - The custom Recents Long Press, if selected.  When null, use default (switch last app).
     private ComponentName mCustomRecentsLongPressHandler = null;
 
+    private int mBlurRadius;
+    private Bitmap mBlurredImage = null;
+
     class SettingsObserver extends UserContentObserver {
         SettingsObserver(Handler handler) {
             super(handler);
@@ -461,6 +465,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     CMSettings.System.NAVBAR_LEFT_IN_LANDSCAPE), false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(CMSettings.System.getUriFor(
                     CMSettings.Secure.RECENTS_LONG_PRESS_ACTIVITY), false, this);
+            resolver.registerContentObserver(NamelessSettings.System.getUriFor(
+                    NamelessSettings.System.LOCKSCREEN_BLUR_RADIUS), false, this,
+                    UserHandle.USER_ALL);
             update();
         }
 
@@ -491,6 +498,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
             // This method reads CMSettings.Secure.RECENTS_LONG_PRESS_ACTIVITY
             updateCustomRecentsLongPressHandler(false);
+
+            mBlurRadius = NamelessSettings.System.getIntForUser(mContext.getContentResolver(),
+                    NamelessSettings.System.LOCKSCREEN_BLUR_RADIUS, 14, UserHandle.USER_CURRENT);
         }
     }
 
@@ -2215,6 +2225,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     && mMediaController.getPlaybackState() != null
                     && mMediaController.getPlaybackState().getState()
                             == PlaybackState.STATE_PLAYING);
+        }
+
+        // apply blurred image
+        if (backdropBitmap == null && !mLiveLockScreenController.isShowingLiveLockScreenView()) {
+            backdropBitmap = mBlurredImage;
+            // might still be null
         }
 
         // apply user lockscreen image
@@ -5373,6 +5389,25 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 || mFingerprintUnlockController.getMode()
                         == FingerprintUnlockController.MODE_WAKE_AND_UNLOCK_PULSING;
         updateDozingState();
+    }
+
+    public void setBackgroundBitmap(Bitmap bmp) {
+        if (bmp == null && mBlurredImage == null) {
+            return;
+        }
+
+        if (bmp != null && mBlurRadius != 0) {
+            mBlurredImage = Blur.blurBitmap(mContext, bmp, mBlurRadius);
+        } else {
+            mBlurredImage = bmp;
+        }
+
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                updateMediaMetaData(true);
+            }
+        });
     }
 
     public VisualizerView getVisualizer() {
