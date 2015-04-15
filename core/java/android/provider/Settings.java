8133b9c7c0d580f6a071e792e1c94dd8be652849
Roman Birg <roman@cyngn.com>
2015-04-15 15:38:50 -0700

    Return of Silent mode (1/2)
    - Adds ability to replace "None" with "Silent" in the Volume panel
    
    - Right now, it will behave exactly the same as "None",
    except allow alarms to go through
    
    Change-Id: Icee44a7114e9529d139c52e5e37e3ec747cec336
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3527,6 +3527,13 @@ public final class Settings {
         public static final String LONG_PRESS_KILL_DELAY = "long_press_kill_delay";
 
         /**
+         * Whether "None" mode will act as "Silent"
+         * Effectively allowing alarms to always use the alarm stream volume when true.
+         * @hide
+         */
+        public static final String NONE_IS_SILENT = "none_is_silent";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
