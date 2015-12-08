240701d2a01c4d1350332000469df103b590deec
Alexander Martinz <eviscerationls@gmail.com>
2015-12-08 15:02:56 +0100

    support nameless resource package
    Change-Id: I91b3d8356cd678dc0ada21710e2465217a3c7e66

--- libs/androidfw/ResourceTypes.cpp
+++ libs/androidfw/ResourceTypes.cpp
@@ -61,6 +61,7 @@ namespace android {
 
 #define APP_PACKAGE_ID      0x7f
 #define CMSDK_PACKAGE_ID    0x3f
+#define NAMELESSSDK_PACKAGE_ID 0x2f
 #define SYS_PACKAGE_ID      0x01
 #define OVERLAY_APP_PACKAGE_ID  0x61
 #define OVERLAY_SYS_PACKAGE_ID  0x60
@@ -6380,6 +6381,7 @@ DynamicRefTable::DynamicRefTable(uint8_t packageId)
     // Reserved package ids
     mLookupTable[APP_PACKAGE_ID] = APP_PACKAGE_ID;
     mLookupTable[SYS_PACKAGE_ID] = SYS_PACKAGE_ID;
+    mLookupTable[NAMELESSSDK_PACKAGE_ID] = NAMELESSSDK_PACKAGE_ID;
     mLookupTable[CMSDK_PACKAGE_ID] = CMSDK_PACKAGE_ID;
     mLookupTable[OVERLAY_APP_PACKAGE_ID] = OVERLAY_APP_PACKAGE_ID;
     mLookupTable[OVERLAY_SYS_PACKAGE_ID] = OVERLAY_SYS_PACKAGE_ID;
@@ -6794,7 +6796,8 @@ bool ResTable::isResTypeAllowed(const char* type) const {
 bool ResTable::isDynamicPackageId(const uint32_t pkgId) const {
     return pkgId != APP_PACKAGE_ID && pkgId != SYS_PACKAGE_ID
             && pkgId != OVERLAY_APP_PACKAGE_ID && pkgId != OVERLAY_SYS_PACKAGE_ID
-            && pkgId != OVERLAY_COMMON_PACKAGE_ID && pkgId != CMSDK_PACKAGE_ID;
+            && pkgId != OVERLAY_COMMON_PACKAGE_ID && pkgId != CMSDK_PACKAGE_ID
+            && pkgId != NAMELESSSDK_PACKAGE_ID;
 }
 
 status_t ResTable::removeIdmappedTypesFromPackageGroup(PackageGroup* packageGroup) const {
