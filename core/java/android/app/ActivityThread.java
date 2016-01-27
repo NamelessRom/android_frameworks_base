a43549f3e0e6a72b15647b2f286311f646d65e8a
Alexander Martinz <eviscerationls@gmail.com>
2016-01-27 13:49:36 +0100

    (Squashed) revert CM density modifications
    Change-Id: I29daa795283f69b66c00888cc20a47df21113006
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -4466,7 +4466,7 @@ public final class ActivityThread {
                     + DisplayMetrics.DENSITY_DEVICE + " to "
                     + mCurDefaultDisplayDpi);
             DisplayMetrics.DENSITY_DEVICE = mCurDefaultDisplayDpi;
-            Bitmap.setDefaultDensity(DisplayMetrics.DENSITY_DEVICE);
+            Bitmap.setDefaultDensity(DisplayMetrics.DENSITY_DEFAULT);
         }
     }
 
