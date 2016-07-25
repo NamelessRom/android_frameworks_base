f0555449e6539fff247b8a21faa68b2a6868d7e8
Alexander Martinz <eviscerationls@gmail.com>
2016-07-25 12:27:11 +0200

    Revert "Tune up dexopt dialog"
    This reverts commit a8a3f096833b60b98f2bac5f280862f733c6f056.
    
    Change-Id: Ib02c8ea066e36fb5241924c18cd730de62ce8f75
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -6560,13 +6560,12 @@ public final class ActivityManagerService extends ActivityManagerNative
     }
 
     @Override
-    public void showBootMessage(final ApplicationInfo info, final int current,
-            final int total, final boolean always) {
+    public void showBootMessage(final CharSequence msg, final boolean always) {
         if (Binder.getCallingUid() != Process.myUid()) {
             // These days only the core system can call this, so apps can't get in
             // the way of what we show about running them.
         }
-        mWindowManager.showBootMessage(info, current, total, always);
+        mWindowManager.showBootMessage(msg, always);
     }
 
     @Override
@@ -11984,8 +11983,8 @@ public final class ActivityManagerService extends ActivityManagerNative
                 intent.setComponent(comp);
                 doneReceivers.add(comp);
                 lastRi = curRi;
-                showBootMessage(ai.applicationInfo, Integer.MIN_VALUE + 2, Integer.MIN_VALUE + 2,
-                        false);
+                CharSequence label = ai.loadLabel(mContext.getPackageManager());
+                showBootMessage(mContext.getString(R.string.android_preparing_apk, label), false);
             }
             Slog.i(TAG, "Pre-boot of " + intent.getComponent().toShortString()
                     + " for user " + users[curUser]);
@@ -12108,7 +12107,9 @@ public final class ActivityManagerService extends ActivityManagerNative
                         synchronized (ActivityManagerService.this) {
                             mDidUpdate = true;
                         }
-                        showBootMessage(null, Integer.MIN_VALUE + 3, Integer.MIN_VALUE + 3, false);
+                        showBootMessage(mContext.getText(
+                                R.string.android_upgrading_complete),
+                                false);
                         writeLastDonePreBootReceivers(doneReceivers);
                         systemReady(goingCallback);
                     }
