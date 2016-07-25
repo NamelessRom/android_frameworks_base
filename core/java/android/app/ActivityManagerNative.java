e9ac0f03aa5893252cb1ded1d4afeaa06612f94e
Alexander Martinz <eviscerationls@gmail.com>
2016-07-25 12:27:11 +0200

    Revert "Tune up dexopt dialog"
    This reverts commit a8a3f096833b60b98f2bac5f280862f733c6f056.
    
    Change-Id: Ib02c8ea066e36fb5241924c18cd730de62ce8f75
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- core/java/android/app/ActivityManagerNative.java
+++ core/java/android/app/ActivityManagerNative.java
@@ -2098,11 +2098,9 @@ public abstract class ActivityManagerNative extends Binder implements IActivityM
 
         case SHOW_BOOT_MESSAGE_TRANSACTION: {
             data.enforceInterface(IActivityManager.descriptor);
-            ApplicationInfo info = ApplicationInfo.CREATOR.createFromParcel(data);
-            int current = data.readInt();
-            int total = data.readInt();
+            CharSequence msg = TextUtils.CHAR_SEQUENCE_CREATOR.createFromParcel(data);
             boolean always = data.readInt() != 0;
-            showBootMessage(info, current, total, always);
+            showBootMessage(msg, always);
             reply.writeNoException();
             return true;
         }
@@ -5285,14 +5283,11 @@ class ActivityManagerProxy implements IActivityManager
         return res;
     }
 
-    public void showBootMessage(ApplicationInfo info, int current, int total,
-            boolean always) throws RemoteException {
+    public void showBootMessage(CharSequence msg, boolean always) throws RemoteException {
         Parcel data = Parcel.obtain();
         Parcel reply = Parcel.obtain();
         data.writeInterfaceToken(IActivityManager.descriptor);
-        info.writeToParcel(data, 0);
-        data.writeInt(current);
-        data.writeInt(total);
+        TextUtils.writeToParcel(msg, data, 0);
         data.writeInt(always ? 1 : 0);
         mRemote.transact(SHOW_BOOT_MESSAGE_TRANSACTION, data, reply, 0);
         reply.readException();
